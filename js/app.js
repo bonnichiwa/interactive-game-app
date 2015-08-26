956b77bc9850fe4caeb3bf19f51519b681048631
bonnichiwa <bonnie.pham@ryerson.ca>
2015-08-26 14:13:38 -0400

    added questions and function to show them

--- js/app.js
+++ js/app.js
@@ -1,4 +1,11 @@
 $(document).ready(function() {
+
+  var currentQuestion = 1
+  var score = 0 
+
+  /*---Clicking start---*/
+
+  function startGame();
   $('#commence-button').click(function() {
     $("#landing-page").fadeOut(1000);
     $("#quiz-interface").fadeIn(1000);
@@ -8,4 +15,48 @@ $(document).ready(function() {
     $("#quiz-interface").fadeOut(1000);
     $("#landing-page").fadeIn(1000);
   })
+
+  /*---Game interface---*/
+
+  function gameInterface();
+    currentQuestion = 1;
+    score = 0;
+    questionIndex = 1;
+    displayQuestion();
+  }
+
+  function displayQuestion() {
+    $(".question").text(quiz[questionIndex].question);
+    $("#option1").text(quiz[questionIndex].choices[0]);
+    $("#option2").text(quiz[questionIndex].choices[1]);
+    $("#option3").text(quiz[questionIndex].choices[2]);
+    $("#option4").text(quiz[questionIndex].choices[3]);
+  }
+  
+  /*--- Questions--- */
+  var quiz = [ 
+
+  {question:"What was the name of Ash Ketchum's first starter Pokemon?",
+  choices: ["Bulbasaur", "Charmander", "Squirtle", "Pikachu"]
+  },
+
+  {question:"What is Kadabra's final evolution?",
+  choices: ["Abra", "Abrakadabra", "Alakazam", "Mega Alakazam"]
+  },
+
+  {question:"How does Chansey evolve into Blissey?",
+  choices: ["Happiness", "Friendship", "Love", "Holding an item"]
+  },
+
+  {question:"Which Pokemon would be the most effective in a battle against Chikorita?",
+  choices: ["Milotic", "Pichu", "Nidoking", "Chimchar"]
+  },
+
+  {question:"What type of Pokemon is Gengar?",
+  choices: ["Ghost", "Poison", "Dark and Ghost", "Poison and Ghost"]
+  }
+
+  ]
+
+
 });
