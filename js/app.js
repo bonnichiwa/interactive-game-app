2bb842d0aa42eaca3ef1a93655f4b0f02010065e
bonnichiwa <bonnie.pham@ryerson.ca>
2015-08-27 12:44:00 -0400

    battle song added upon click into game interface(keeps disconnecting sublime)

--- js/app.js
+++ js/app.js
@@ -9,6 +9,8 @@ $(document).ready(function() {
     $('#commence-button').click(function() {
     $("#landing-page").fadeOut(1000);
     $("#quiz-interface").fadeIn(1000);
+    $("#opening-song")[0].pause();
+    battleSong();
     gameInterface();
   })
 
