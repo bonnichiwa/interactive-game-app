3723f528ac3e7c681cee639a382f765e390e276b
bonnichiwa <bonnie.pham@ryerson.ca>
2015-08-27 17:50:48 -0400

    positioned explanation images

--- js/app.js
+++ js/app.js
@@ -88,9 +88,9 @@ $(document).ready(function() {
           .addClass("wrong");
         }
         questionIndex++;
-        $(".explanation[value=" + questionIndex + "]").fadeIn(1000);
+        $(".explanation[value=" + questionIndex + "]").fadeIn(500);
         $(".explanation[value=" + questionIndex + "]").click(function() {
-          $(".explanation[value=" + questionIndex + "]").fadeOut(1000);
+          $(".explanation[value=" + questionIndex + "]").fadeOut(500);
           displayQuestion();
         })
       } else {
